db7dd96bee2ab7b55b24bac5ee03a87f6c51e20a
codeajay <ajay01937@gmail.com>
2024-03-21 13:10:42 +0530

    first commit

---
+++ app/src/main/java/com/example/expensetracker/ExpensesAdapter.java
@@ -0,0 +1,75 @@
+package com.example.expensetracker;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpensesAdapter extends RecyclerView.Adapter<ExpensesAdapter.MyViewHolder> {
+    private Context context;
+    private OnItemsClick onItemsClick;
+    private List<ExpenseModel> expenseModelList;
+
+    public ExpensesAdapter(Context context, OnItemsClick onItemsClick) {
+        this.context = context;
+        expenseModelList = new ArrayList<ExpenseModel>();
+        this.onItemsClick = onItemsClick;
+    }
+    public void add(ExpenseModel expenseModel){
+        expenseModelList.add(expenseModel);
+        notifyDataSetChanged();
+    }
+    public void clear(){
+        expenseModelList.clear();
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.expense_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        ExpenseModel expenseModel = expenseModelList.get(position);
+        holder.note.setText(expenseModel.getNote());
+        holder.category.setText(expenseModel.getCategory());
+        holder.amount.setText(String.valueOf(expenseModel.getAmount()));
+
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                onItemsClick.onClick(expenseModel);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return expenseModelList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView note, category, date, amount;
+        public MyViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+            note = itemView.findViewById(R.id.note);
+            category = itemView.findViewById(R.id.category);
+            date = itemView.findViewById(R.id.date);
+            amount = itemView.findViewById(R.id.amount);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -0,0 +1,150 @@
+package com.example.expensetracker;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+//import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.expensetracker.databinding.ActivityMainBinding;
+import com.example.expensetracker.databinding.ActivityMainBinding;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnItemsClick{
+    ActivityMainBinding binding;
+    private ExpensesAdapter expenseAdapter;
+    private long income = 0, expense = 0;
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding =  ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        expenseAdapter = new ExpensesAdapter(this, this);
+        binding.recycler.setAdapter(expenseAdapter);
+        binding.recycler.setLayoutManager(new LinearLayoutManager(this));
+
+        intent = new Intent(MainActivity.this, AddExpenseActivity.class);
+
+        binding.addIncome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.putExtra("type", "Income");
+                startActivity(intent);
+            }
+        });
+        binding.addExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.putExtra("type", "Expense");
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please");
+        progressDialog.setMessage("Wait");
+        progressDialog.setCancelable(false);
+        if (FirebaseAuth.getInstance().getCurrentUser() == null){
+            progressDialog.show();
+            FirebaseAuth.getInstance()
+                    .signInAnonymously()
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            progressDialog.cancel();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.cancel();
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        income = 0;
+        expense = 0;
+        getData();
+    }
+    private void getData(){
+        FirebaseFirestore
+                .getInstance()
+                .collection("expenses")
+                .whereEqualTo("uid", FirebaseAuth.getInstance().getUid())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        expenseAdapter.clear();
+                        List<DocumentSnapshot> dsList = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot ds:dsList){
+                            ExpenseModel expenseModel = ds.toObject(ExpenseModel.class);
+                            if (expenseModel.getType().equals("Income")){
+                                income += expenseModel.getAmount();
+                            }else{
+                                expense += expenseModel.getAmount();
+                            }
+                            expenseAdapter.add(expenseModel);
+                        }
+                        setUpGraph();
+                    }
+                });
+    }
+
+    private void setUpGraph() {
+        List<PieEntry> pieEntryList = new ArrayList<>();
+        List<Integer> colorsList = new ArrayList<>();
+        if (income != 0){
+            pieEntryList.add(new PieEntry(income, "Income"));
+            colorsList.add(getResources().getColor(R.color.green));
+        }
+        if (expense != 0){
+            pieEntryList.add(new PieEntry(expense, "Expense"));
+            colorsList.add(getResources().getColor(R.color.yellow));
+        }
+        PieDataSet pieDataSet = new PieDataSet(pieEntryList, String.valueOf(income - expense));
+        pieDataSet.setColors(colorsList);
+        pieDataSet.setValueTextColor(getResources().getColor(R.color.white));
+        PieData pieDat = new PieData( pieDataSet);
+
+        binding.pieChart.setData(pieDat);
+        binding.pieChart.invalidate();
+    }
+
+    @Override
+    public void onClick(ExpenseModel expenseModel) {
+        intent.putExtra("model",expenseModel);
+        startActivity(intent);
+    }
+}
